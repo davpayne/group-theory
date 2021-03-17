8190eb3e9efaa8dce775a7d6bd99fde6203311b5
David <dpayne162@gmail.com>
2021-03-16 20:39:20 -0400

    fixed object tests

--- Julia_GroupTheory/runtests.jl
+++ Julia_GroupTheory/runtests.jl
@@ -1,9 +1,10 @@
 using Test
+include("object_definitions.jl")
 
 @testset "objects defined" begin
-    test_actor = actor(1)
-    @test test_actor.id == 1
-    
-    test_stage = stage(1)
-    @test test_stage.id == 1
+    test_worker = worker(1,1,1)
+    @test test_worker.id == 1
+
+    test_task = task(1,1,1,1)
+    @test test_task.id == 1
 end
